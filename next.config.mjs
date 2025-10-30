0d8d46d8240e35017abe633e80a4e5b9562421cc
Héctor Toro <asdxdd7@gmail.com>
2025-10-29 22:25:57 -0300

    Standar deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: { typedRoutes: true },
-  // Export estático (Next 13/14 App Router)
-  output: 'export',
-  // Necesario si en el futuro usas next/image con export
-  images: { unoptimized: true },
+  // quita: output: 'export'
+  images: { /* si quieres volver a optimizar */ }
 };
-
 export default nextConfig;
