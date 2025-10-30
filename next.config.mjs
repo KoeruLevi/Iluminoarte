db25164535bcdaa9785c582476d7bd0ba9f1d5f2
Héctor Toro <asdxdd7@gmail.com>
2025-10-29 22:17:30 -0300

    Arreglos deployment vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
+  experimental: { typedRoutes: true },
+  // Export estático (Next 13/14 App Router)
+  output: 'export',
+  // Necesario si en el futuro usas next/image con export
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
